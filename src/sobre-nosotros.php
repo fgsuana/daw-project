a6b7f398df7a2969337b82364567ce6b96402f5d
Angel Pérez <angelperezmdev@gmail.com>
2023-05-23 21:12:44 +0200

    Pagina Sobre Nosotros

--- src/sobre-nosotros.php
+++ src/sobre-nosotros.php
@@ -10,45 +10,111 @@
     <title>Sobre Nosotros</title>
 </head>
 <body>
-    <?php include 'header.php'; ?>
+<?php include 'header.php'; ?>
 
-    <header class="py-5">
-    <div class="container">
-      <h1 class="text-center">Sobre nosotros</h1>
+<header class="py-5">
+<div class="container">
+  <h1 class="text-center">Sobre nosotros</h1>
+</div>
+</header>
+
+<section>
+<div class="container">
+  <p>Somos un apasionado y dedicado equipo que se esfuerza por ofrecer soluciones innovadoras y creativas para satisfacer las necesidades de nuestros clientes.</p>
+  <div class="row mt-5">
+    <div class="col-lg-6 d-flex flex-column justify-content-center align-items-center">
+      <h2>Nuestra historia</h2>
+      <p>Somos un equipo de tres jóvenes entusiastas que estamos realizando nuestro proyecto final del CFGS de Desarrollo de Aplicaciones Web. Desde el inicio de nuestra formación, nos hemos sumergido en el mundo del desarrollo web, adquiriendo habilidades en programación, diseño y gestión de proyectos.</p>
+      <p>A lo largo de nuestro recorrido, nos hemos enfrentado a desafíos técnicos que nos han permitido crecer y mejorar nuestras habilidades. Ahora, con nuestro proyecto final, queremos llevar nuestras capacidades al siguiente nivel y demostrar todo lo que hemos aprendido.</p>
+      <p>Nuestro objetivo es desarrollar una aplicación web funcional, intuitiva y visualmente atractiva. Nos enfocamos en brindar una experiencia de usuario excepcional, priorizando la usabilidad y la eficiencia.</p>
+      <p>Estamos emocionados de presentar nuestro proyecto final y compartir todo el conocimiento y la pasión que hemos invertido en él. Esperamos que nuestra historia refleje nuestro compromiso con el desarrollo web y la creación de soluciones innovadoras.</p>
     </div>
-  </header>
+    <div class="col-lg-6 imgsobrenosotros">
+    </div>
+  </div>
 
-  <section class="py-5">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6">
-          <h2>Nuestra historia</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim auctor mauris, id hendrerit nunc mattis ac. Nulla laoreet gravida purus, eget eleifend velit. Sed sed arcu non turpis tristique hendrerit nec a ante. Aliquam quis augue vel tellus pulvinar rutrum vel a turpis.</p>
+  <div class="row mt-5 mb-5">
+    <div class="col-lg-6">
+      <h2>Nuestros Valores</h2>
+      <div class="accordion accordion-flush" id="accordionFlushExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingOne">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+            Excelencia
+            </button>
+          </h2>
+          <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Nos esforzamos por alcanzar la excelencia en todo lo que hacemos, brindando productos/servicios de alta calidad y superando las expectativas de nuestros clientes.</div>
+          </div>
         </div>
-        <div class="col-lg-6">
-          <h2>Nuestro equipo</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim auctor mauris, id hendrerit nunc mattis ac. Nulla laoreet gravida purus, eget eleifend velit. Sed sed arcu non turpis tristique hendrerit nec a ante. Aliquam quis augue vel tellus pulvinar rutrum vel a turpis.</p>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingTwo">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+            Innovación
+            </button>
+          </h2>
+          <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Impulsamos la innovación constante, buscando nuevas ideas y soluciones creativas para mantenernos a la vanguardia de nuestro sector y adaptarnos a las cambiantes necesidades del mercado.</div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingThree">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+            Integridad
+            </button>
+          </h2>
+          <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Valoramos la honestidad, la transparencia y la ética en todas nuestras interacciones, construyendo relaciones sólidas y duraderas con nuestros clientes, colaboradores y socios.</div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingFour">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+            Trabajo en equipo
+            </button>
+          </h2>
+          <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Fomentamos un ambiente colaborativo, donde el trabajo en equipo y la comunicación efectiva son fundamentales para lograr resultados exitosos. Valoramos y respetamos las contribuciones individuales mientras trabajamos juntos hacia un objetivo común.</div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingFive">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+            Pasión por el servicio
+            </button>
+          </h2>
+          <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Nos apasiona brindar un servicio excepcional a nuestros clientes. Nos esforzamos por comprender sus necesidades, anticiparnos a sus deseos y superar sus expectativas en cada interacción, ofreciendo una experiencia única y satisfactoria.</div>
+          </div>
         </div>
       </div>
+    </div>
 
-      <div class="row">
-        <div class="col-lg-6">
-          <h2>Nuestra misión</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim auctor mauris, id hendrerit nunc mattis ac. Nulla laoreet gravida purus, eget eleifend velit. Sed sed arcu non turpis tristique hendrerit nec a ante. Aliquam quis augue vel tellus pulvinar rutrum vel a turpis.</p>
+
+    <div class="col-lg-6 text-center">
+      <h2>Nosotros</h2>
+      <div class="row imagenesnosotros mt-4">
+        <div class="col-md-4">
+          <h5>Angel</h5>
+          <img src="../src/img/angel.jpg" alt="Angel" class="img-nosotros">
         </div>
-        <div class="col-lg-6">
-          <h2>Nuestros valores</h2>
-          <ul>
-            <li>Compromiso</li>
-            <li>Innovación</li>
-            <li>Colaboración</li>
-            <li>Integridad</li>
-          </ul>
+        <div class="col-md-4">
+          <h5>Joel</h5>
+          <img src="../src/img/joel.png" alt="Joel" class="img-nosotros">
+        </div>
+        <div class="col-md-4">
+          <h5>Giancarlo</h5>
+          <img src="../src/img/giancarlo.png" alt="Giancarlo" class="img-nosotros">
         </div>
       </div>
     </div>
-  </section>
-    
+
+
+  </div>
+</div>
+</section>
+
+
 
 
   <?php include_once './footer.php'; ?>
