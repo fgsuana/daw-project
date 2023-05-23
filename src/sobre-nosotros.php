e89ead2b6abd93c570bf3fa132226ee45ead51d1
Joel Pérez Miralles <63650751+joelprz92@users.noreply.github.com>
2023-05-23 20:54:15 +0200

    Update sobre-nosotros.php

--- src/sobre-nosotros.php
+++ src/sobre-nosotros.php
@@ -51,20 +51,4 @@
     
 
 
-    <footer>
-
-        <div class="social-icons"><i class="ejemplo fa-brands fa-facebook"></i> <i class="fa-brands fa-instagram"></i> <i class="fa-brands fa-youtube"></i> <i class="fa-brands fa-linkedin"></i> </div>
-        <div class="legalfooter">
-            <span>2023 Proyecto</span>
-            <span><a href="./src/aviso-legal.php">Aviso legal</a></span>
-            <span><a href="./src/politica-cookies.php">Política de cookies</a></span>
-            <span><a href="./src/politica-privacidad.php">Política de privacidad</a></span>
-        </div>
-
-    </footer>
-
-
-    <button class="fas fa-arrow-up" onclick="goToTop()" id="myButton" title="Ir arriba"></button>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
-</body>
-</html>
+  <?php include_once './footer.php'; ?>
