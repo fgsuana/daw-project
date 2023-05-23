4b2a070c8ec9da95a0c81e514661d8955d39fee2
Joel Pérez Miralles <63650751+joelprz92@users.noreply.github.com>
2023-05-23 19:10:28 +0200

    Update buscar.php

--- src/buscar.php
+++ src/buscar.php
@@ -1,4 +1,17 @@
-    <?php
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="../node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+    <title>Contacto</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+<?php
     // Verificar si se ha enviado una consulta
     if (isset($_GET['consulta'])) {
         // Obtener la consulta del formulario
