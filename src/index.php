cd1a17d02dbb29373ecc25aafc5119ff6b8f5b5d
Joel Pérez Miralles <63650751+joelprz92@users.noreply.github.com>
2023-05-23 20:53:33 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -12,7 +12,7 @@
 
 <body>
 
-    <?php include './header.php'; ?>
+    <?php include_once './header.php'; ?>
     <!-- Inicio banner --><section class="spikes">
     <div class="bannerimg">
     <h2 class="h2subtitlemain">Más oportunidades laborales, mejores condiciones</h3>
@@ -69,16 +69,7 @@
     </div>
 
 
-    <footer>
-        <div class="social-icons"><a href="https://www.facebook.com/devmatchweb"><i class="ejemplo fa-brands fa-facebook"></i></a> <i class="fa-brands fa-instagram"></i> <i class="fa-brands fa-youtube"></i> <i class="fa-brands fa-linkedin"></i> </div>
-        <div class="legalfooter">
-            <span>2023 Proyecto</span>
-            <span><a href="./aviso-legal.php">Aviso legal</a></span>
-            <span><a href="./politica-cookies.php">Política de cookies</a></span>
-            <span><a href="./politica-privacidad.php">Política de privacidad</a></span>
-        </div>
 
-    </footer>
 
 <!-- Inicio Cookies -->
 
@@ -110,18 +101,5 @@
     </div>
 </div>
 
+<?php include_once './footer.php'; ?>
 
-<button class="fas fa-arrow-up" onclick="goToTop()" id="myButton" title="Ir arriba"></button>
-<script>
-
-</script>
-<!-- Final Cookies -->
-
-    <script src="./js/scripts.js"></script>
-    <script type="module" src="./js/main.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
