02e108ce07cdd40a09b162ccc2dc9afaaa8af1dd
Joel Pérez Miralles <63650751+joelprz92@users.noreply.github.com>
2023-05-23 21:36:21 +0200

    Update and rename registro.html to registro.php

--- src/registro.php
+++ src/registro.php
@@ -7,8 +7,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilos.css">
     <link rel="stylesheet" href="../node_modules/@fortawesome/fontawesome-free/css/all.min.css">
-    <title>Registro</title>
+    <title>Sobre Nosotros</title>
 </head>
+<body>
+<?php include 'header.php'; ?>
+
 <body class="body-registro">
 
     <section>
@@ -69,6 +72,5 @@
     </section>
 
     <script type="module" src="js/registro.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
-</body>
-</html>
+
+    <?php include_once './footer.php'; ?>
