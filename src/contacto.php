3dd1b3c037f05a97d8de3e7a96bcc0f96d2bb636
Joel Pérez Miralles <63650751+joelprz92@users.noreply.github.com>
2023-05-23 20:52:20 +0200

    Update contacto.php

--- src/contacto.php
+++ src/contacto.php
@@ -1,100 +1,105 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <title>Portal de trabajo</title>
     <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../node_modules/@fortawesome/fontawesome-free/css/all.min.css">
-    <title>Contacto</title>
 </head>
+
 <body>
-    <?php include 'header.php'; ?>
 
-    <div class="row text-center divscontact">
-        <!-- Caja 1 = Telefono -->
-        <div class="col-md-4 boxcontact d-flex flex-column justify-content-center align-items-center">
-            <i class="fas fa-phone"></i>
-            <h5 class="text-center">Teléfono</h5>
-            <p class="text-center">+34 123456789</p>
+    <?php include_once './header.php'; ?>
+    <!-- Inicio banner --><section class="spikes">
+    <div class="bannerimg">
+    <h2 class="h2subtitlemain">Más oportunidades laborales, mejores condiciones</h3>
+    <p class="subtitlemain">Conviértete en el profesional IT que tanto deseas</p>
+        <div class="d-flex h-100 align-items-center justify-content-center">
+            <div id="hero-image">
+                <form method="get" action="buscar.php" class="buscadorhome">
+                    <span class="iconoinput"><img src="./img/macbook.svg" alt="macbook" height="30px"/></span>
+                    <input type="text" name="buscadortrabajo" placeholder="Buscar proyecto...">
+                    <input type="submit" value="Voy a tener suerte">
+                </form>
+            </div>
         </div>
+    </div></section>
+    <!-- Fin banner -->
 
+    <main class="mainbox">
+        <div class="mainhomejobs">
+            <div class="cajahomejobs">
+                <div><span class="mainhomejobtitle">¿Te gustaría crecer como profesional en IT?</span></div>
+                <div><span class="mainhomejobsubtitle">Desarrolla tus habilidades profesionales con el proyecto que más se adapte a tus inquietudes.</span></div> 
 
-        <!-- Caja 2 = Correo -->
-        <div class="col-md-4 boxcontact d-flex flex-column justify-content-center align-items-center">
-            <i class="fas fa-envelope"></i>
-            <h5>Correo</h5>
-            <p>ejemplo@info.com</p>
+                    <div class="steps">
+                        <div class="step"><img src="https://detallesorballo.com/wp-content/uploads/2020/09/imagen-de-prueba-320x240-1.jpg"></div>
+                        <div class="step"><img src="https://detallesorballo.com/wp-content/uploads/2020/09/imagen-de-prueba-320x240-1.jpg"></div>
+                        <div class="step"><img src="https://detallesorballo.com/wp-content/uploads/2020/09/imagen-de-prueba-320x240-1.jpg"></div>
+                    </div>
+            </div>   
         </div>
-
-        <!-- Caja 3 = Dirección -->
-        <div class="col-md-4 boxcontact d-flex flex-column justify-content-center align-items-center">
-            <i class="fas fa-map-marker-alt"></i>
-            <h5>Dirección</h5>
-            <p>C. de Pelai, 7, 08001 Barcelona</p>
-        </div>
-
-    </div>
-
-    <div class="padremapform">
-        <!-- Caja 1 = Mapa -->
-        <div class="imagencontact d-flex flex-column justify-content-center align-items-center">
-            <h4>¡Contáctanos!</h4>
-            <p>Si tienes dudas solo contáctanos y te atenderemos lo antes posiible</p>
-        </div>
-
-
-
         
-        <!-- Caja 2 = Formulario -->
-        <div class="col-md-6 fromcontact">
-            
-            <div class="col-md-6">
-                <h4>¡Contacta con nosotros!</h4>
-            </div>
+        <h2>Últimos proyectos publicados</h2>
 
-            <div class="form50">
-                <div class="col-md-6">
-                    <input type="text" class="form-control text-start" placeholder="Nombre">
-                </div>
-                <div class="col-md-6">
-                    <input type="text" class="form-control text-start" placeholder="Correo">
-                </div>
-            </div>
-            <div class="form50">
-                <div class="col-md-6">
-                    <input type="text" class="form-control text-start" placeholder="Teléfono">
-                </div>
-                <div class="col-md-6">
-                    <input type="text" class="form-control text-start" placeholder="Asunto">
-                </div>
-            </div>
-            <div>
-                <div>
-                    <textarea class="form-control text-start" cols="30" rows="15" placeholder="Mensaje"></textarea>
-                </div>
-            </div>
 
-        </div>
+        <section class="trabajos">
 
-    </div>
+        </section>
+
+        <h2>¿En qué rama te gustaría participar?</h2>
+        <section class="trabajos rama">
+            <div class="trabajo dessoft"><div class="titlerama"><h3>Desarollo de software</h3></div></div>
+            <div class="trabajo ingred"><div class="titlerama"><h3>Ingeniería de redes</h3></div></div>
+            <div class="trabajo seginf"><div class="titlerama"><h3>Seguridad informática</h3></div></div>
+            <div class="trabajo adbd"><div class="titlerama"><h3>Administración de BD</h3></div></div>
+            <div class="trabajo andat"><div class="titlerama"><h3>Análisis de datos</h3></div></div>
+            <div class="trabajo adsist"><div class="titlerama"><h3>Administrador de sistemas</h3></div></div>
+        </section>
     
+        <h3>Entérate de las últimas novedades del sector IT en nuestro blog</h3>
+         
+    </main>
+    <div class="prefooter spikes">
+        <h3 class="prefooterh3">Trabajamos con las mejores marcas</h3>
+        <img src="https://d2gma3rgtloi6d.cloudfront.net/e930ee29/images/main/main-header-logos-mobile@2x.png" />
+    </div>
 
 
-    <footer>
 
-        <div class="social-icons"><i class="ejemplo fa-brands fa-facebook"></i> <i class="fa-brands fa-instagram"></i> <i class="fa-brands fa-youtube"></i> <i class="fa-brands fa-linkedin"></i> </div>
-        <div class="legalfooter">
-            <span><a href="/aviso-legal.php">Aviso legal</a></span>
-            <span><a href="/politica-cookies.php">Política de cookies</a></span>
-            <span><a href="/politica-privacidad.php">Política de privacidad</a></span>
+
+<!-- Inicio Cookies -->
+
+<!-- Banner de cookies -->
+<div class="fixed-bottom py-2 cookiesbanner" id="cookieBanner">
+    <div class="container d-flex justify-content-center align-items-center flex-wrap">
+        <span class="text-center m-2 colorspan">En nuestro sitio web utilizamos cookies para brindarle una experiencia más personalizada y eficiente. Estas cookies nos permiten recordar sus preferencias y hábitos de navegación para que podamos ofrecerle contenido y servicios relevantes para usted.</span>
+        <div class="mt-2">
+            <button type="button" class="masinfocookies me-2" data-bs-toggle="modal" data-bs-target="#cookieModal">Más Información</button>
+            <button type="button" class="aceptarcookies" id="acceptCookies">Aceptar</button>
         </div>
+    </div>
+</div>
 
-    </footer>
+<!-- Modal de configuración de cookies -->
+<div class="modal fade" id="cookieModal" tabindex="-1" aria-labelledby="cookieModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="cookieModalLabel">INFORMACIÓN ADICIONAL</h5>
+            </div>
+            <div class="modal-body">
+                <p>Las cookies son pequeños archivos de texto que se almacenan en su dispositivo cuando utiliza nuestro sitio web. Utilizamos cookies necesarias para el correcto funcionamiento del sitio y para mejorar su experiencia de navegación. También utilizamos cookies de análisis para recopilar información sobre cómo utiliza nuestro sitio web y para mejorar nuestros servicios. Puede elegir si desea permitir o no el uso de cookies no esenciales. Tenga en cuenta que el bloqueo de ciertos tipos de cookies puede afectar su experiencia de navegación en nuestro sitio web. Para obtener más información sobre cómo utilizamos las cookies, consulte nuestra Política de cookies.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="botoncerrar" data-bs-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
 
+<?php include_once './footer.php'; ?>
 
-    <button class="fas fa-arrow-up" onclick="goToTop()" id="myButton" title="Ir arriba"></button>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
-</body>
-</html>
