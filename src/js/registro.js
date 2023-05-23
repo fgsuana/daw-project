1b2e8a8943072d7661124e9b5900db8efe5b5a11
Joel Pérez Miralles <63650751+joelprz92@users.noreply.github.com>
2023-05-23 21:50:15 +0200

    Update registro.js

--- src/js/registro.js
+++ src/js/registro.js
@@ -1,37 +1,41 @@
 import { getProfiles } from './api.js';
- let profiles = [];
+let profiles = [];
 
-   // Función para pintar los perfiles en la pantalla
-   const renderProfiles = () => {
-    const checkboxContainer = document.getElementById('checkbox-container');
-  
-    // Limpiar el contenedor antes de agregar los nuevos perfiles
-    checkboxContainer.innerHTML = '';
-  
-    // Recorrer el array de perfiles y crear los elementos de checkbox
-    profiles.forEach((profile) => {
-      const checkbox = document.createElement('input');
-      checkbox.classList.add('checkboxprofiles');
-      checkbox.type = 'checkbox';
-      checkbox.value = profile.id;
-      checkbox.name = 'profile';
-      checkbox.id = `profile-${profile.id}`;
-  
-      const label = document.createElement('label');
-      label.textContent = profile.nombre_perfil;
-      label.htmlFor = `profile-${profile.id}`;
-  
-      checkboxContainer.appendChild(checkbox);
-      checkboxContainer.appendChild(label);
-    });
-  };
+// Función para pintar los perfiles en la pantalla
+const renderProfiles = () => {
+  const checkboxContainer = document.getElementById('checkbox-container');
 
+  // Limpiar el contenedor antes de agregar los nuevos perfiles
+  checkboxContainer.innerHTML = '';
 
+  // Recorrer el array de perfiles y crear los elementos de checkbox
+  profiles.forEach((profile) => {
+    const valorInputPerfilDiv = document.createElement('div');
+    valorInputPerfilDiv.classList.add('valorinputperfil');
 
-    window.addEventListener('load', () => {
-        getData();
-      });
-      const getData = async () => {
-        profiles = await getProfiles('../backend');
-        renderProfiles();
-      };
+    const checkbox = document.createElement('input');
+    checkbox.classList.add('checkboxprofiles');
+    checkbox.type = 'checkbox';
+    checkbox.value = profile.id;
+    checkbox.name = 'profile';
+    checkbox.id = `profile-${profile.id}`;
+
+    const label = document.createElement('label');
+    label.textContent = profile.nombre_perfil;
+    label.htmlFor = `profile-${profile.id}`;
+
+    valorInputPerfilDiv.appendChild(checkbox);
+    valorInputPerfilDiv.appendChild(label);
+
+    checkboxContainer.appendChild(valorInputPerfilDiv);
+  });
+};
+
+window.addEventListener('load', () => {
+  getData();
+});
+
+const getData = async () => {
+  profiles = await getProfiles('../backend');
+  renderProfiles();
+};
