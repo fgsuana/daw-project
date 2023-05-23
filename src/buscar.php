01737ba342c39fd690f3872c0e8831f797db8639
Joel Pérez Miralles <63650751+joelprz92@users.noreply.github.com>
2023-05-23 20:52:03 +0200

    Update buscar.php

--- src/buscar.php
+++ src/buscar.php
@@ -70,34 +70,4 @@
 
         </main>
 
-<footer>
-        <div class="social-icons"><a href="https://www.facebook.com/devmatchweb"><i class="ejemplo fa-brands fa-facebook"></i></a> <i class="fa-brands fa-instagram"></i> <a href="https://www.youtube.com/@Devmatch-DesarolloWeb-sz2uj"><i class="fa-brands fa-youtube"></i></a> <a href="https://www.linkedin.com/company/devmatchweb/?original_referer="><i class="fa-brands fa-linkedin"></i></a> </div>
-        <div class="legalfooter">
-            <span>2023 Proyecto</span>
-            <span><a href="./src/aviso-legal.php">Aviso legal</a></span>
-            <span><a href="./src/politica-cookies.php">Política de cookies</a></span>
-            <span><a href="./src/politica-privacidad.php">Política de privacidad</a></span>
-        </div>
-
-    </footer>
-
-<!-- Inicio Cookies -->
-
-<button class="fas fa-arrow-up" onclick="goToTop()" id="myButton" title="Ir arriba"></button>
-<script>
-
-</script>
-<!-- Final Cookies -->
-
-<?php
-$directorioPrincipal = $_SERVER['DOCUMENT_ROOT'];
-?>
-
-<script src="<?php echo $directorioPrincipal; ?>/js/scripts.js"></script>
-<script type="module" src="<?php echo $directorioPrincipal; ?>/js/main.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
+        <?php include_once './footer.php'; ?>
