2969a1bea8d5017f4440218f0acf69dc227eb3d1
Joel Pérez Miralles <63650751+joelprz92@users.noreply.github.com>
2023-05-24 18:39:24 +0200

    Update pubproyecto.php

--- src/pubproyecto.php
+++ src/pubproyecto.php
@@ -17,34 +17,34 @@
     
     <div class="inputformproyecto d-flex align-items-center justify-content-center" style="height: 100vh;">
         <div class="container">
-            <h1 class="text-center">Formulario para crear un proyecto</h1>
+            <h1 class="whitecolor text-center">Formulario para crear un proyecto</h1>
             <form action="#" method="POST">
             <div class="form-group text-center">
-                <label class="mt-3" for="nombre-proyecto">Nombre del proyecto</label>
+                <label class="mt-3" for="whitecolor nombre-proyecto">Nombre del proyecto</label>
                 <input type="text" class="form-control" id="nombre-proyecto" name="nombre-proyecto" required>
             </div>
         
             <div class="form-group text-center">
-                <label class="mt-3" for="puestos-libres">Puestos libres o desarrolladores necesarios</label>
-                <input type="text" class="form-control" id="puestos-libres" name="puestos-libres" required>
+                <label class="whitecolor mt-3" for="puestos-libres">Puestos libres o desarrolladores necesarios</label>
+                <input type="text" class="whitecolor form-control" id="puestos-libres" name="puestos-libres" required>
             </div>
         
             <div class="form-group text-center">
-                <label class="mt-3" for="categoria-proyecto">Categoría del proyecto</label>
-                <select class="form-control" id="categoria-proyecto" name="categoria-proyecto" required>
-                <option class="text-center" value="" selected>Seleccione una categoría</option>
-                <option class="text-center" value="Front-end">Front-end</option>
-                <option class="text-center" value="Back-end">Back-end</option>
+                <label class="whitecolor mt-3" for="categoria-proyecto">Categoría del proyecto</label>
+                <select class="form-control whitecolor" id="categoria-proyecto" name="categoria-proyecto" required>
+                <option class="text-center whitecolor" value="" selected>Seleccione una categoría</option>
+                <option class="text-center whitecolor" value="Front-end">Front-end</option>
+                <option class="text-center whitecolor" value="Back-end">Back-end</option>
                 </select>
             </div>
         
             <div class="form-group text-center">
-                <label class="mt-3" for="descripcion-proyecto">Descripción del proyecto</label>
+                <label class="mt-3 whitecolor" for="descripcion-proyecto">Descripción del proyecto</label>
                 <textarea class="form-control" id="descripcion-proyecto" name="descripcion-proyecto" rows="5" required></textarea>
             </div>
             
             <div class="text-center">
-                <button type="submit" class="botonenviar mt-3">Crear proyecto</button>
+                <button type="submit" class="botonenviar mt-3 whitecolor">Crear proyecto</button>
             </div>
             </form>
         </div>
