609463c86f277ae244adc678f0af8e38b47d795c
Joel Pérez Miralles <63650751+joelprz92@users.noreply.github.com>
2023-05-24 18:37:42 +0200

    Update pubproyecto.php

--- src/pubproyecto.php
+++ src/pubproyecto.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="../node_modules/@fortawesome/fontawesome-free/css/all.min.css">
 </head>
 
-<body class="publicarproyecto">
+<body class="principal">
     <header>
     <?php include 'header.php'; ?>
     </header>
