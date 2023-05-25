5bfa314a38678661cdc5099d29d6e6ad27e89207
Joel Pérez Miralles <63650751+joelprz92@users.noreply.github.com>
2023-05-25 12:14:15 +0200

    Update terminos-condiciones.php

--- src/terminos-condiciones.php
+++ src/terminos-condiciones.php
@@ -15,9 +15,68 @@
     <?php include 'header.php'; ?>
 
     <main class="mainbox">
-        <h1>Términos y condiciones</h1>
 	
+        <body>
+    <h1>Términos y Condiciones de Devmatch</h1>
+    <p><strong>1. Aceptación de los Términos</strong></p>
+    <p>1.1 Usted debe leer y aceptar estos Términos antes de utilizar nuestro Servicio. Su uso continuado del Servicio
+        constituye su aceptación de estos Términos y cualquier modificación futura.</p>
 
+    <p><strong>2. Descripción del Servicio</strong></p>
+    <p>2.1 Devmatch es una plataforma en línea que permite a los desarrolladores unirse a proyectos colaborativos y
+        mejorar sus habilidades profesionales. Proporcionamos un espacio para que los desarrolladores encuentren
+        oportunidades de colaboración, formen equipos y compartan conocimientos.</p>
+    <p>2.2 El Servicio incluye, entre otras cosas, perfiles de usuario, proyectos, foros de discusión, mensajería y
+        contenido generado por los usuarios.</p>
+
+    <p><strong>3. Registro de la Cuenta</strong></p>
+    <p>3.1 Para acceder a ciertas funciones del Servicio, debe crear una cuenta. Al registrarse, usted acepta proporcionar
+        información precisa, completa y actualizada.</p>
+    <p>3.2 Usted es responsable de mantener la confidencialidad de su contraseña y de todas las actividades que ocurran en
+        su cuenta. Usted acepta notificarnos inmediatamente cualquier uso no autorizado de su cuenta o cualquier otra
+        violación de seguridad.</p>
+
+    <p><strong>4. Uso Apropiado</strong></p>
+    <p>4.1 Usted se compromete a utilizar el Servicio de manera adecuada y de acuerdo con la ley aplicable. No debe utilizar
+        el Servicio para ningún propósito ilegal, difamatorio, obsceno, abusivo, ofensivo, fraudulento o perjudicial
+        para otros usuarios o terceros.</p>
+    <p>4.2 Usted acepta no interferir ni interrumpir el funcionamiento adecuado del Servicio, incluyendo, pero no
+        limitado a, el uso de cualquier virus, robot, spider, scraper u otros medios automatizados para acceder al
+        Servicio o extraer información de él.</p>
+
+    <p><strong>5. Propiedad Intelectual</strong></p>
+    <p>5.1 El Servicio y su contenido, incluyendo pero no limitado a textos, gráficos, logotipos, nombres comerciales,
+        marcas registradas, imágenes, videos, software y otros materiales, están protegidos por derechos de autor,
+        marcas registradas y otros derechos de propiedad intelectual.</p>
+    <p>5.2 Usted reconoce y acepta que no adquiere ningún derecho de propiedad sobre el contenido que se muestra en el
+        Servicio y se compromete a respetar los derechos de propiedad intelectual de Devmatch y de terceros.</p>
+
+    <p><strong>6. Limitación de Responsabilidad</strong></p>
+    <p>6.1 El uso del Servicio es bajo su propio riesgo. No garantizamos que el Servicio sea ininterrumpido, seguro, libre
+        de errores o esté libre de virus u otros componentes dañinos. No seremos responsables de ningún daño directo,
+        indirecto, incidental, especial o consecuente que surja del uso o la imposibilidad de usar el Servicio.</p>
+
+    <p><strong>7. Modificaciones del Servicio y los Términos</strong></p>
+    <p>7.1 Nos reservamos el derecho de modificar o discontinuar el Servicio en cualquier momento, sin previo aviso.
+        También nos reservamos el derecho de modificar estos Términos en cualquier momento. Cualquier modificación
+        entrará en vigencia tan pronto como se publique en el Servicio.</p>
+
+    <p><strong>8. Terminación</strong></p>
+    <p>8.1 Usted puede cancelar su cuenta en cualquier momento mediante la función de cancelación de cuenta disponible en
+        el Servicio. Nos reservamos el derecho de suspender o cancelar su cuenta si viola estos Términos o si
+        determinamos, a nuestra sola discreción, que su conducta es perjudicial para el Servicio o para otros
+        usuarios.</p>
+
+    <p><strong>9. Ley Aplicable</strong></p>
+    <p>9.1 Estos Términos se regirán e interpretarán de acuerdo con las leyes del país en el que operamos.</p>
+
+    <p><strong>10. Contacto</strong></p>
+    <p>10.1 Si tiene alguna pregunta o inquietud acerca de estos Términos, puede ponerse en contacto con nosotros a través
+        de los medios proporcionados en el Servicio.</p>
+
+    <p>¡Gracias por utilizar Devmatch! Esperamos que disfrute de nuestra plataforma y tenga una experiencia positiva en su
+        desarrollo profesional.</p>
+</body>
         
 
     </main>
