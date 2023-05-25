db5864e87253290c281fbe4c1ba7830bcd681aee
Joel Pérez Miralles <63650751+joelprz92@users.noreply.github.com>
2023-05-25 12:10:27 +0200

    Update terminos-condiciones.php

--- src/terminos-condiciones.php
+++ src/terminos-condiciones.php
@@ -1,1 +1,24 @@
 
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Términos y condiciones</title>
+    <link rel="stylesheet" href="./css/estilos.css">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <main class="mainbox">
+        <h1>Términos y condiciones</h1>
+	
+
+        
+
+    </main>
+    <?php include_once './footer.php'; ?>
